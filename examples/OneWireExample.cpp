0fc42681569ad23dd25f057c3cf9322bfc39e71d
azhel12 <azhel12reg@gmail.com>
2021-01-24 10:32:45 +0300

    Added driver (and example) for ds18b20 temperature sensor. Fix 1-wire example.

--- examples/OneWireExample.cpp
+++ examples/OneWireExample.cpp
@@ -1,4 +1,6 @@
-#define F_CPU 8000000
+// For stm32f401 HSI_VALUE is 16Mhz. It's important for USART configuration.
+#define HSI_VALUE 16000000
+#define F_CPU 16000000
 
 #include <one_wire.h>
 #include <usart.h>
@@ -10,7 +12,7 @@ using Line = OneWire<Usart1, IO::Pa9>;
 int main()
 {
     // This example detect address (ROM) of one connected device.
-    uint8_t rom[8] = {};
+    uint8_t rom[8] = {0x00};
     Line::Init();
     Line::ReadRom(rom);
     // Now @rom array contains address of sensor. You can use it in future.
