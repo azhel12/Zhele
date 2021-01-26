39cfded08a9986e76d1bcd7d9feb3ebc1f626a43
azhel12 <azhel12reg@gmail.com>
2021-01-26 20:19:14 +0300

    Callback types edited to std::function (instead pointer to function). It takes some ROM/RAM. Mb should edit back.

--- Zhele/include/common/template_utils/data_transfer.h
+++ Zhele/include/common/template_utils/data_transfer.h
@@ -11,13 +11,17 @@
 #define ZHELE_DATATRANSFER_H
 
 #include <type_traits>
-
+#include <functional>
 namespace Zhele
 {
-	/// Transfer callback pointer
-	using TransferCallback = std::add_pointer_t<void(void* data, unsigned size, bool success)>;
-	/// Tagged transfer callback pointer
-	using TaggedTransferCallback = std::add_pointer_t<void(void* tag, void* data, unsigned size, bool success)>;
+    // Using std::function insted pointer takes about 300 bytes flash and 60 bytes RAM.
+    // Therefore, it's big question: std::function or pointer.
+    /// Transfer callback pointer
+    //using TransferCallback = std::add_pointer_t<void(void* data, unsigned size, bool success)>;
+    using TransferCallback = std::function<void(void* data, unsigned size, bool success)>;
+    /// Tagged transfer callback pointer
+    //using TaggedTransferCallback = std::add_pointer_t<void(void* tag, void* data, unsigned size, bool success)>;
+    using TaggedTransferCallback = std::function<void(void* tag, void* data, unsigned size, bool success)>;
 }
 
 #endif //!ZHELE_DATATRANSFER_H
