2e8f666bf3166c62f067dccfb7590337415ab47e
azhel12 <azhel12reg@gmail.com>
2023-03-17 15:20:16 +0300

    Fixed iopin for modern template_utils

--- Zhele/include/common/impl/iopin.h
+++ Zhele/include/common/impl/iopin.h
@@ -43,13 +43,13 @@ namespace Zhele::IO
 	template<typename _Port, uint8_t _Pin, typename _ConfigPort>
 	void TPin<_Port, _Pin, _ConfigPort>::Toggle()
 	{
-		Port::Toggle(1u << _Pin);
+		Port::template Toggle<1u << _Pin>();
 	}
 
 	template<typename _Port, uint8_t _Pin, typename _ConfigPort>
 	void TPin<_Port, _Pin, _ConfigPort>::SetDirRead()
 	{
-		Port::template SetPinConfiguration<_Pin>(Port::In);
+		Port::template SetConfiguration(1u << _Pin, _Port::Configuration::In);
 	}
 
 	template<typename _Port, uint8_t _Pin, typename _ConfigPort>
@@ -61,84 +61,84 @@ namespace Zhele::IO
 	template<typename _Port, uint8_t _Pin, typename _ConfigPort>
 	void TPin<_Port, _Pin, _ConfigPort>::SetConfiguration(Configuration configuration)
 	{
-		_ConfigPort::SetConfiguration(1u << _Pin, configuration);
+		_ConfigPort::SetConfiguration(configuration, 1u << _Pin);
 	}
 
 	template<typename _Port, uint8_t _Pin, typename _ConfigPort>
 	template<typename TPin<_Port, _Pin, _ConfigPort>::Configuration configuration>
 	void TPin<_Port, _Pin, _ConfigPort>::SetConfiguration()
 	{
-		_ConfigPort::template SetConfiguration<1u << _Pin, configuration>();
+		_ConfigPort::template SetConfiguration<configuration, 1u << _Pin>();
 	}
 	
 	template<typename _Port, uint8_t _Pin, typename _ConfigPort>
 	void TPin<_Port, _Pin, _ConfigPort>::SetDriverType(DriverType driverType)
 	{
-		_ConfigPort::SetDriverType(1u << _Pin, driverType);
+		_ConfigPort::SetDriverType(driverType, 1u << _Pin);
 	}
 	
 	template<typename _Port, uint8_t _Pin, typename _ConfigPort>
 	template<typename TPin<_Port, _Pin, _ConfigPort>::DriverType driverType>
 	void TPin<_Port, _Pin, _ConfigPort>::SetDriverType()
 	{
-		_ConfigPort::template SetDriverType<1u << _Pin, driverType>();
+		_ConfigPort::template SetDriverType<driverType, 1u << _Pin>();
 	}
 
 	template<typename _Port, uint8_t _Pin, typename _ConfigPort>
 	void TPin<_Port, _Pin, _ConfigPort>::SetPullMode(PullMode pullMode)
 	{
-		_ConfigPort::SetPullMode(1u << _Pin, pullMode);
+		_ConfigPort::SetPullMode(pullMode, 1u << _Pin);
 	}
 
 	template<typename _Port, uint8_t _Pin, typename _ConfigPort>
 	template<typename TPin<_Port, _Pin, _ConfigPort>::PullMode pullMode>
 	void TPin<_Port, _Pin, _ConfigPort>::SetPullMode()
 	{
-		_ConfigPort::template SetPullMode<1u << _Pin, pullMode>();
+		_ConfigPort::template SetPullMode<pullMode, 1u << _Pin>();
 	}
 	
 	template<typename _Port, uint8_t _Pin, typename _ConfigPort>
 	void TPin<_Port, _Pin, _ConfigPort>::SetSpeed(Speed speed)
 	{
-		_ConfigPort::SetSpeed(1u << _Pin, speed);
+		_ConfigPort::SetSpeed(speed, 1u << _Pin);
 	}
 
 	template<typename _Port, uint8_t _Pin, typename _ConfigPort>
 	template<typename TPin<_Port, _Pin, _ConfigPort>::Speed speed>
 	void TPin<_Port, _Pin, _ConfigPort>::SetSpeed()
 	{
-		_ConfigPort::template SetSpeed<1u << _Pin, speed>();
+		_ConfigPort::template SetSpeed<speed, 1u << _Pin>();
 	}
 
 	template<typename _Port, uint8_t _Pin, typename _ConfigPort>
 	void TPin<_Port, _Pin, _ConfigPort>::AltFuncNumber(uint8_t funcNumber)
 	{
-		_ConfigPort::AltFuncNumber(1u << _Pin, funcNumber);
+		_ConfigPort::AltFuncNumber(funcNumber, 1u << _Pin);
 	}
 
 	template<typename _Port, uint8_t _Pin, typename _ConfigPort>
 	template<uint8_t funcNumber>
 	void TPin<_Port, _Pin, _ConfigPort>::AltFuncNumber()
 	{
-		_ConfigPort::template AltFuncNumber<1u << _Pin, funcNumber>();
+		_ConfigPort::template AltFuncNumber<funcNumber, 1u << _Pin>();
 	}
 
 	template<typename _Port, uint8_t _Pin, typename _ConfigPort>
 	bool TPin<_Port, _Pin, _ConfigPort>::IsSet()
 	{
-		return (Port::PinRead() & (typename Port::DataType)(1u << _Pin)) != 0;
+		return (Port::PinRead() & static_cast<typename Port::DataType>(1u << _Pin)) != 0;
 	}
 
 	template<typename _Port, uint8_t _Pin, typename _ConfigPort>
 	void TPin<_Port, _Pin, _ConfigPort>::WaitForSet()
 	{
-		while(IsSet()==0){}
+		while (!IsSet()) continue;
 	}
 
 	template<typename _Port, uint8_t _Pin, typename _ConfigPort>
 	void TPin<_Port, _Pin, _ConfigPort>::WaitForClear()
 	{
-		while(IsSet()){}
+		while (IsSet()) continue;
 	}
 
 	template<typename _Port, uint8_t _Pin, typename _ConfigPort>
