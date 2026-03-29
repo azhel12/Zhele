d43ea892ce512a88ec150bccfcaf37660c3ea42d
Alexey Zhelonkin <azhel12reg@gmail.com>
2026-03-29 10:44:44 +0300

    Removed unused interrupts define and declarations.h headerfile

--- include/zhele/common/macro_utils/declarations.h
+++
@@ -1,8 +0,0 @@
-#ifndef ZHELE_DECLARATIONS_H
-#define ZHELE_DECLARATIONS_H
-
-#if defined (__GNUC__) && defined(__arm__)
-#define ZHELE_INTERRUPT(ISR_NAME) __attribute__((interrupt)) void ISR_NAME()
-#endif
-
-#endif //!ZHELE_DECLARATIONS_H
